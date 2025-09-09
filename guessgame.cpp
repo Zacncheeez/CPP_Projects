406eaad3d9df7231d77959267c09433d21039101
U-BSD\453682 <453682@SHS-FYBF6X3.beaverton.k12.or.us>
2025-09-09 10:19:24 -0700

    save

--- guessgame.cpp
+++ guessgame.cpp
@@ -1,7 +1,34 @@
-include <iostream>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
+
+
 int main() {
-  cout << "Hello";
+  cout << "Welcome to guessing game! Guess a number from 1-100" << endl;
+
+  //int num1 = readInt("What is your guess?: ");
+  //cout << num1 << endl;
+
+  int answer = 50; 
+  int guess;
+    
+  cout << "What is your guess?: " << endl;
+  cin >> guess;	  
+  cout << "Your guess is: " << guess << endl;
+	  
+  if (answer < guess) {
+    cout << "Guess is too high" << endl;
+  }
+
+  else if (answer > guess) {
+    cout << "Guess is too low" << endl;
+  }
+  else {
+    cout << "You win! Congratulations!" << endl;
+  }
+
+
+	  
   return 0;
 }
