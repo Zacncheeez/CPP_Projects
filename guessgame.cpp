d2b60fd6ece1c40e90f8360b5dd0666cfd9f7ac6
U-BSD\453682 <453682@SHS-FYBF6X3.beaverton.k12.or.us>
2025-09-11 10:19:41 -0700

    save

--- guessgame.cpp
+++ guessgame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 using namespace std;
 
 
@@ -10,25 +11,37 @@ int main() {
   //int num1 = readInt("What is your guess?: ");
   //cout << num1 << endl;
 
-  int answer = 50; 
+  
+  int guessCount = 0;
+  
+  srand(time(NULL));
+  int answer = rand()% 100; 
   int guess;
-    
-  cout << "What is your guess?: " << endl;
-  cin >> guess;	  
-  cout << "Your guess is: " << guess << endl;
+
+  for (;;) {
+  
+    std::cout << "What is your guess?: " << endl;
+    std::cin >> guess;	  
+    std::cout << "Your guess is: " << guess << endl;
+ 
 	  
-  if (answer < guess) {
-    cout << "Guess is too high" << endl;
-  }
+    if (answer < guess) {
+      cout << "Guess is too high" << endl;
+      guessCount = guessCount += 1;
+    }
 
-  else if (answer > guess) {
-    cout << "Guess is too low" << endl;
-  }
-  else {
-    cout << "You win! Congratulations!" << endl;
-  }
+    else if (answer > guess) {
+      cout << "Guess is too low" << endl;
+      guessCount = guessCount += 1;
+    }
+    else if (answer = guess) {
+      cout << "You win! Congratulations!" << endl;
+      cout << "Guesses: "<< guessCount << endl;
+      break;
+    }
 
+  }
+  
 
-	  
   return 0;
 }
