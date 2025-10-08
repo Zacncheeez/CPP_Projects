b85d32d6e66565a4ba8bb71add3789456eb7a676
U-BSD\453682 <453682@SHS-FYBF6X3.beaverton.k12.or.us>
2025-10-08 10:06:44 -0700

    save

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -18,7 +18,33 @@ Citations:
 
 using namespace std;
 
+
+  bool checkWinner(char arr[4][4], char player) {
+    //check rows
+    for (int i = 1; i <= 3; i++) {
+        if (arr[i][1] == player && arr[i][2] == player && arr[i][3] == player)
+            return true;
+    }   
+    //check columns
+    for (int j = 1; j <= 3; j++) {
+        if (arr[1][j] == player && arr[2][j] == player && arr[3][j] == player)
+            return true;
+    }
+    //check diagnals
+    if (arr[1][1] == player && arr[2][2] == player && arr[3][3] == player) {
+        return true;
+    }
+    if (arr[3][1] == player && arr[2][2] == player && arr[1][3] == player) {
+        return true;
+    }
+    
+    return false;
+  }
+
 int main() {
+  
+  for(;;) {
+  
   cout << "Lets play TicTacToe" << endl;
 
   char arr[4][4] = {
@@ -29,7 +55,7 @@ int main() {
   };
   
   char i, j;
-
+  
   
   for(;;) {
 
@@ -40,22 +66,52 @@ int main() {
         }
         cout << endl;
     }
-
+    
     
   int rowChoice;
   int columnChoice;
+  char currentPlayer;
 
   cout << "What column? ";
   cin >> columnChoice;
   
   cout << "What row? ";
   cin >> rowChoice;
-  
+
   cout << "X or O: ";
-  cin >> arr[rowChoice][columnChoice];
- 
+  cin >> currentPlayer; 
+
+  
+    arr[rowChoice][columnChoice] = currentPlayer;
+    if (checkWinner(arr, currentPlayer)) {
+        cout << "\nThe Board is:\n";
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < 4; j++) {
+                    cout << "\t" << arr[i][j];
+                }
+        cout << endl;
+    }
+        cout << "Player " << currentPlayer << " wins!" << endl;
+        break;
+     
+  }
+  }
+  
+    char again;
+        cout << "Want to play again? (y/n)" << endl;
+        cin >> again;
+
+        if (again == 'y') {
+          continue;
+        }
+
+        else {
+	  cout << "OK";
+          break;
+        }
+
   }  
   
-
+  cout << "Thanks for playing!";
   return 0;
 }
